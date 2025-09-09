70ca3d13c1c1eb5d581180a196db8ef92170ce22
Luke Harwood <lukeharwood11@gmail.com>
2025-09-08 20:39:14 -0500

    Update

--- supabase/migrations/20250909005110_create_requests_table.sql
+++ supabase/migrations/20250909005110_create_requests_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS relationships (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id_1 UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    user_id_2 UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    user_id_1 UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE, -- requester
+    user_id_2 UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE, -- requested
     relationship_type TEXT NOT NULL, -- 'family', 'friend', 'partner', 'colleague', 'other' (this can be normalized later)
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
