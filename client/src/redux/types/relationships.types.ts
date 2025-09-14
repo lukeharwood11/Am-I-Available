621f4ad1afaa3424080f69573f8bc9043152c9a3
Luke Harwood <lukeharwood11@gmail.com>
2025-09-13 23:07:31 -0500

    Add todos

--- client/src/redux/types/relationships.types.ts
+++ client/src/redux/types/relationships.types.ts
@@ -2,6 +2,10 @@ import { RelationshipData } from '../hubs/relationships.hub';
 import { RelationshipRequestData } from '../hubs/relationship-requests.hub';
 
 // Relationship State Types
+// TODO: move this out of this file and into the slice file
+// TODO: fix the hubs to return the actual filled out data (not just IDs)
+// TODO: use supabase client where applicable
+// TODO: add the actual types for relationship objects here
 export interface RelationshipState {
   relationships: RelationshipData[];
   relationshipRequests: {
