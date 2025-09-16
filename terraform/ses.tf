25c6afd593201aacebb94c0b2c4f1403c85c6f41
Luke Harwood <lukeharwood11@gmail.com>
2025-09-15 22:18:48 -0500

    Fix SES config... I'm dumb and should have put everything in us-east-1 to begin with

--- terraform/ses.tf
+++ terraform/ses.tf
@@ -19,6 +19,7 @@ resource "aws_route53_record" "amia_domain_txt" {
   allow_overwrite = true
   records = [
     aws_ses_domain_identity.amia_domain_identity.verification_token,
+    aws_ses_domain_identity.amia_domain_identity_us_east_1.verification_token,
     "v=spf1 include:amazonses.com ~all"
   ]
 }
@@ -28,7 +29,7 @@ resource "aws_route53_record" "amia_domain_mx" {
   name    = aws_ses_domain_identity.amia_domain_identity.domain
   type    = "MX"
   ttl     = "3600"
-  records = ["10 feedback-smtp.us-east-2.amazonses.com"]
+  records = ["10 inbound-smtp.us-east-1.amazonaws.com"]
 }
 
 resource "aws_ses_domain_mail_from" "amia_domain_mail_from" {
@@ -43,6 +44,16 @@ resource "aws_route53_record" "amia_dkim_records" {
   type    = "CNAME"
   ttl     = "3600"
   records = ["${aws_ses_domain_dkim.amia_domain_dkim.dkim_tokens[count.index]}.dkim.amazonses.com"]
+}
+
+# DKIM records for us-east-1 (for email receiving)
+resource "aws_route53_record" "amia_dkim_records_us_east_1" {
+  count   = 3
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "${aws_ses_domain_dkim.amia_domain_dkim_us_east_1.dkim_tokens[count.index]}._domainkey"
+  type    = "CNAME"
+  ttl     = "3600"
+  records = ["${aws_ses_domain_dkim.amia_domain_dkim_us_east_1.dkim_tokens[count.index]}.dkim.amazonses.com"]
 }
 
 resource "aws_route53_record" "amia_mail_from_mx" {
@@ -121,23 +132,34 @@ resource "aws_s3_bucket_policy" "amia_email_bucket_policy" {
   })
 }
 
-# SES Receipt Rule Set for incoming emails
-resource "aws_ses_receipt_rule_set" "amia_receipt_rule_set" {
-  rule_set_name = "amia-email-rules"
+# SES Domain Identity for email receiving (must be in us-east-1)
+resource "aws_ses_domain_identity" "amia_domain_identity_us_east_1" {
+  provider = aws.us_east_1
+  domain   = local.domain_name
 }
 
-# Set the receipt rule set as active
-resource "aws_ses_active_receipt_rule_set" "amia_active_rule_set" {
-  rule_set_name = aws_ses_receipt_rule_set.amia_receipt_rule_set.rule_set_name
+# Domain verification for us-east-1
+resource "aws_ses_domain_identity_verification" "amia_domain_identity_verification_us_east_1" {
+  provider = aws.us_east_1
+  domain   = aws_ses_domain_identity.amia_domain_identity_us_east_1.domain
 }
 
-# Receipt rule to handle emails sent to any address at amiavailable.com
+# DKIM for us-east-1
+resource "aws_ses_domain_dkim" "amia_domain_dkim_us_east_1" {
+  provider = aws.us_east_1
+  domain   = aws_ses_domain_identity.amia_domain_identity_us_east_1.domain
+}
+
+# Receipt rule to handle emails sent to amia@amiavailable.com
+# Add to existing INBOUND_MAIL rule set in us-east-1
 resource "aws_ses_receipt_rule" "amia_catch_all_rule" {
-  name          = "catch-all-emails"
-  rule_set_name = aws_ses_receipt_rule_set.amia_receipt_rule_set.rule_set_name
+  provider      = aws.us_east_1
+  name          = "amia-emails"
+  rule_set_name = "INBOUND_MAIL"
   recipients    = ["amia@amiavailable.com"]
   enabled       = true
-  scan_enabled  = false // TODO: enable this later
+  scan_enabled  = false
+  after         = "m-184cb351d99d46eab737badfa7b19f89"  # Add after existing WorkMail rule
 
   s3_action {
     bucket_name       = aws_s3_bucket.amia_email_bucket.bucket
@@ -145,5 +167,8 @@ resource "aws_ses_receipt_rule" "amia_catch_all_rule" {
     position          = 1
   }
 
-  depends_on = [aws_s3_bucket_policy.amia_email_bucket_policy]
+  depends_on = [
+    aws_s3_bucket_policy.amia_email_bucket_policy,
+    aws_ses_domain_identity_verification.amia_domain_identity_verification_us_east_1
+  ]
 }
