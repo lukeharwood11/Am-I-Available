36f96b453487ab2120ec4b54474a6032ee895dca
Luke Harwood <lukeharwood11@gmail.com>
2025-09-17 07:43:32 -0500

    Fix project name

--- terraform/openai.tf
+++ terraform/openai.tf
@@ -3,6 +3,6 @@ resource "openai_project" "amia" {
 }
 
 resource "openai_project_service_account" "amia" {
-  project_id = openai_project.example.id
-  name       = "my-service-account"
+  project_id = openai_project.amia.id
+  name       = "amia-api"
 }
